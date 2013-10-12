317d7de2be747c1f6dd8b277ff47f6a65e3aac79
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2013-10-12 11:40:35 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,3 +1,5 @@
+//Problem 6: Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 #include<iostream>
 #include<cmath>
 using namespace std;
