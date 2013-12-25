a59479b0ec0a77ee085cb164f7caafe307663222
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2013-12-25 14:31:05 +0530

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -1,3 +1,9 @@
+/*Longest Collatz Sequence
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+Which starting number, under one million, produces the longest chain?
+*/
+
 #include<iostream>
 using namespace std;
 
